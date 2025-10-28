ea7b68f608ef266fc0c6e7928fed867703b186a6
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-10-28 02:11:20 -0700

    add flake output package

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
-  description = "nixos proxmox fleet managed by colmena";
+  description = "Jellarr flake";
 
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
     treefmt-nix = {
@@ -11,15 +12,40 @@
   };
 
   outputs = {
+    flake-parts,
     nixpkgs,
-    systems,
     treefmt-nix,
     ...
-  }: {
-    formatter = let
-      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
-      treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
-    in
-      eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
-  };
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = {system, ...}: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        formatter = (treefmt-nix.lib.evalModule pkgs ./treefmt.nix).config.build.wrapper;
+
+        packages.default = pkgs.buildGoModule {
+          pname = "jellarr";
+          version = "0.1.0";
+          src = ./.;
+          subPackages = ["cmd/jellarr"];
+          vendorHash = "sha256-m6zzY4lhxmfMxkmaF09/7Hiiwx0MSYO+Rbuj6bW7H4s=";
+          ldflags = ["-s" "-w"];
+          doCheck = true;
+          meta = with pkgs.lib; {
+            description = "Declarative Jellyfin configuration engine (typed Go client)";
+            license = licenses.agpl3Only;
+            homepage = "https://github.com/venkyr77/jellarr";
+            mainProgram = "jellarr";
+            platforms = platforms.linux ++ platforms.darwin;
+          };
+        };
+      };
+    };
 }
